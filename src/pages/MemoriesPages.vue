45a3439ae51019704d34ca5a2345ffc147ac578d
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-13 10:07:29 +0200

    memories page

---
+++ src/pages/MemoriesPages.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+      <ion-header>
+      <ion-toolbar>
+      <ion-title>TED Nigga</ion-title>
+      </ion-toolbar>
+      </ion-header>
+      <ion-content>
+          <h2>All star Game</h2>
+      </ion-content>
+  </ion-page>  
+</template>
+<script>
+import { IonPage,IonHeader,IonToolbar, IonTitle,IonContent } from'@ionic/vue';
+
+export default ({
+     components:{
+     IonPage,
+     IonHeader,
+     IonToolbar,
+     IonTitle,
+     IonContent 
+
+     }   
+
+})
+</script>
+
