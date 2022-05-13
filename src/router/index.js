9fae531fe6196f08483f4ba07ede94072b8ad661
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-13 10:07:59 +0200

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,18 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import MemoriesPages from '../pages/MemoriesPages.vue';
 
 const routes = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/memories'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
+    path: '/memories',
+    component: MemoriesPages,
   }
-]
+  
+      
+    ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
