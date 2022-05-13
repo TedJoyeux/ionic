5ba967d0f88a6f66b24c0d1c0705ecc1141863bc
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-13 19:40:27 +0200

    memories

--- src/pages/MemoriesDetails.vue
+++ src/pages/MemoriesDetails.vue
@@ -1,5 +1,5 @@
 <template>
-    <base-layout page-title="Details">
+    <base-layout page-title="Details" page-default-back-link="/memories">
     <h2>Ted Nigga</h2>
     </base-layout>
 </template>
