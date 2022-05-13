e1b3af043b094c84a727516c5d6b88afbd2ae06d
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-13 14:28:50 +0200

    baselayout

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+      <ion-header>
+      <ion-toolbar>
+          <ion-buttons slot="start">
+              <ion-back-button></ion-back-button>
+          </ion-buttons>
+      <ion-title>{{pageTitle}}</ion-title>
+      </ion-toolbar>
+      </ion-header>
+      <ion-content>
+         <slot />
+      </ion-content>
+  </ion-page>   
+</template>
+<script>
+import { IonPage,IonHeader,IonToolbar, IonTitle,IonContent,IonBackButton,IonButtons } from'@ionic/vue';
+ export default{
+     props:['pageTitle'],
+     components:{
+     IonPage,
+     IonHeader,
+     IonToolbar,
+     IonTitle,
+     IonContent ,
+     IonBackButton,
+     IonButtons
+
+
+
+    
+     } 
+ }
+</script>
