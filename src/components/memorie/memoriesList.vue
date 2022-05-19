094ea96894c3ddf296b95d1103cf62d69bdf8925
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-19 10:53:55 +0200

    memorylist

--- src/components/memorie/memoriesList.vue
+++ src/components/memorie/memoriesList.vue
@@ -1,10 +1,19 @@
 <template>
     <ion-list>
-        
-      </ion-list>
+     <memory-list-item  v-for="memory in memories" 
+     :key='memory.id'
+     :memory="memory"
+     ></memory-list-item>   
+    </ion-list>
 </template>
 <script>
+import { IonList } from '@ionic/vue'
+import memoriesListItem from './memoriesListItem.vue'
 export default{
-
+    props:['memories'],
+     components:{
+         IonList,
+         memoriesListItem,
+     }
 }
 </script>
