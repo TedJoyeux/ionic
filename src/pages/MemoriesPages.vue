c8c729af2339675caedf9aeb0108b90e7c718da5
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-19 09:34:06 +0200

    memopages

--- src/pages/MemoriesPages.vue
+++ src/pages/MemoriesPages.vue
@@ -1,29 +1,15 @@
 <template>
   <base-layout page-title="All Memories">
-      <ion-list>
-        <ion-item  v-for="memory in memories" :router-link="`/memories/${memory.id}`" :key='memory.id'>
-        <ion-thumbnail slot="start">
-        <ion-img :src="memory.image"></ion-img>
-        </ion-thumbnail>
-        <ion-label>
-        {{memory.title}}
-        </ion-label>
-        </ion-item>
-      </ion-list>
+      
   </base-layout>
  
 </template>
 <script>
-import { IonList, IonItem,IonImg, IonThumbnail,IonLabel} from'@ionic/vue';
+import {  } from'@ionic/vue';
 import BaseLayout from '../components/BaseLayout.vue';
 
 export default ({
      components:{
-     IonList, 
-     IonItem,
-     IonThumbnail,
-     IonLabel,
-     IonImg,
      BaseLayout
      },   
     computed:{
