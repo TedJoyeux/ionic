012c6d7394abe91a5067f0bfd34ebb25baa74e5b
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-19 09:33:24 +0200

    memo

---
+++ src/components/memorie/memoriesListItem.vue
@@ -0,0 +1,20 @@
+<template>
+   <ion-item  v-for="memory in memories" :router-link="`/memories/${memory.id}`" :key='memory.id'>
+        <ion-thumbnail slot="start">
+        <ion-img :src="memory.image"></ion-img>
+        </ion-thumbnail>
+        <ion-label>
+        {{memory.title}}
+        </ion-label>
+        </ion-item> 
+</template>
+<script>
+import {IonItem, IonThumbnail , IonLabel} from '@ionic/vue'
+export default{
+   components:{
+       IonItem,
+       IonThumbnail,
+       IonLabel
+   }
+}
+</script>
