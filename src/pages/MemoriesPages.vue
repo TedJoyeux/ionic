6da4203c98035047f202a93ea5ca5a465d8eae8d
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-19 10:54:24 +0200

    memorypages

--- src/pages/MemoriesPages.vue
+++ src/pages/MemoriesPages.vue
@@ -1,16 +1,17 @@
 <template>
   <base-layout page-title="All Memories">
-      
+  <memories-list :memories="memories"></memories-list>
   </base-layout>
  
 </template>
 <script>
-import {  } from'@ionic/vue';
+import memoriesList from '../components/memorie/memoriesList.vue';
 import BaseLayout from '../components/BaseLayout.vue';
 
 export default ({
      components:{
-     BaseLayout
+     BaseLayout,
+     memoriesList,
      },   
     computed:{
       memories(){
