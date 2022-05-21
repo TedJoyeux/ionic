50bac05158bb5f0fd11d2503ca9ea0dff03e5b9b
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-21 11:23:04 +0200

    memoryOverview

---
+++ src/components/memorie/MemoryOverview.vue
@@ -0,0 +1,14 @@
+<template>
+  <ion-img :scr="image" :alt="title"></ion-img>  
+  <h2 class="ion-text-center">{{ title }}</h2>
+  <p class="ion-text-center">{{ description }}</p>
+</template>
+<script>
+import { IonImg } from '@ionic/vue';
+export default{
+   props:['image','title','description'],
+   components:{
+     IonImg,
+   }
+}
+</script>
