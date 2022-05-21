e6a248197b0ff06b91f22cc6fb6e5d6e4d6aae11
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-21 11:24:02 +0200

    memory details

--- src/pages/MemoriesDetails.vue
+++ src/pages/MemoriesDetails.vue
@@ -1,16 +1,24 @@
 <template>
     <base-layout :page-title="loadedMemory ? loadedMemory.title:'loading..'" page-default-back-link="/memories">
     <h2 v-if="!loadedMemory">could not find memory</h2>
-    <h2 v-else>Loaded..</h2>
+    <memory-overview v-else 
+    :image="loadedMemory.image" 
+    :title="loadedMemory.title"
+    :description="loadedMemory.description">
+    </memory-overview>
     <h2>Ted Nigga</h2>
     </base-layout>
 </template>
 <script>
+import MemoryOverview from '../components/memorie/MemoryOverview.vue'
 export default{
     data(){
      return{
          memoryId: this.$route.params.id,
      }
+    },
+    components:{
+     MemoryOverview
     },
     computed:{
      loadedMemory(){
