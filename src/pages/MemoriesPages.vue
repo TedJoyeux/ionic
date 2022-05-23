f084020f2cdf14defb7068e0e0f5ca777f8ac258
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-23 12:25:19 +0200

    memoriespages

--- src/pages/MemoriesPages.vue
+++ src/pages/MemoriesPages.vue
@@ -1,10 +1,17 @@
 <template>
   <base-layout page-title="All Memories">
+  <template v-slot:actions-end>
+    <ion-button router-link="/memories/add">
+      <ion-icon slot="icon-only" :icon="add"></ion-icon>
+    </ion-button>
+  </template>
   <memories-list :memories="memories"></memories-list>
   </base-layout>
  
 </template>
 <script>
+import { IonButton, IonIcon } from '@ionic/vue';
+import { add } from 'ionicons/icons'
 import memoriesList from '../components/memorie/memoriesList.vue';
 import BaseLayout from '../components/BaseLayout.vue';
 
@@ -12,7 +19,12 @@ export default ({
      components:{
      BaseLayout,
      memoriesList,
-     },   
+     IonButton,
+     IonIcon
+     },  
+     data(){
+       return{ add }
+     } ,
     computed:{
       memories(){
         return this.$store.getters.memories;
