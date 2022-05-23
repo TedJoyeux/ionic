92ee96efcf5440f11111932d47648220c4cc3464
TedJoyeux <impundujoyeuxthevenin@gmail.com>
2022-05-23 12:24:56 +0200

    addmemory

--- src/pages/AddMemory.vue
+++ src/pages/AddMemory.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1></h1> 
+  <base-layout page-title=" Add Memory" page-default-back-link="/memories">
+  <h2> Add a memory</h2>
+
+  </base-layout> 
 </template>
 <script>
 export default{
